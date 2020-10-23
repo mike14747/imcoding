0110dfbe7af8973ee6a9bb96ebd241b6c023d608
mike14747 <mike4747@oh.rr.com>
2020-10-23 13:15:57 -0400

    Ran a new production build with the new logo.

---
+++ client/build/precache-manifest.ece7a5439553fb72d91858d4b67177f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9b787405670216661817c3c5d2d0cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c44b40466163560ee813",
+    "url": "/static/css/main.60331165.chunk.css"
+  },
+  {
+    "revision": "0f2047fb358de2c6e8e7",
+    "url": "/static/js/2.b04f1e15.chunk.js"
+  },
+  {
+    "revision": "05d5bb7b37cd264eb188b9bb0ea9612c",
+    "url": "/static/js/2.b04f1e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44b40466163560ee813",
+    "url": "/static/js/main.a6310f5a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "22c1253b426b7845fb3fb32b963914f5",
+    "url": "/static/media/loading.22c1253b.gif"
+  }
+]);

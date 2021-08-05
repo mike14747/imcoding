58f50099ce9d377ed3feb752d6609d6f4df92668
mike14747 <mike4747@oh.rr.com>
2021-08-05 14:53:29 -0400

    Added /client/.env. Got the frontend testing environment configured and working with a few basic tests for now.

--- client/src/__tests__/components/footer.test.js
+++ client/src/__tests__/components/footer.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { expect } from '@jest/globals';
 
 import Footer from '../../components/footer/footer';
 
 describe('Test the footer component', () => {
+    jest.resetModules();
+
     test('Make sure all 3 parts of the footer render', () => {
         render(<Footer />);
 
-        expect(1).toBe(1);
+        console.log('process.env.REACT_APP_EMAIL:', process.env.REACT_APP_EMAIL);
+
+        expect(screen.getByText('Let me know if you have any questions, comments or info on how to improve any of the code in these articles.')).toBeInTheDocument();
+        expect(screen.getByText(/I'd love to know how you do it!/)).toBeInTheDocument();
+
+        const contact = screen.getByRole('link');
+        expect(contact).toHaveTextContent(/Contact Me/i);
+        expect(contact).toHaveAttribute('href', 'mailto:mike4747@oh.rr.com');
+
+        expect(screen.getByText('© 2020 imcoding')).toBeInTheDocument();
     });
 });

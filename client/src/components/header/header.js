8ac52ebb8bad837eca922daca6ddf65051952260
mike14747 <mike4747@oh.rr.com>
2021-09-02 22:45:02 -0400

    Added a new logo. Restyled the header and footer a bit. Added a background image to the app.

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -6,15 +6,24 @@ import './css/header.css';
 const Header = () => {
     return (
         <header className="header">
-            <div className="header-container">
-                <div className="header-left">
+            <div className="header-left">
+                <div className="logo-container">
                     <Link to="/">
-                        <img src="images/imcoding_logo3.png" alt="Home" className="logo" />
+                        <img src="/images/imcoding_logo4.png" alt="ImCoding" className="logo" />
                     </Link>
                 </div>
-                <div className="header-right">
-                    <Dropdown />
+
+                <div className="heading-container">
+                    <h1 className="heading">
+                        ImCoding
+                    </h1>
+                    <p className="slogan">
+                        WebDev Articles
+                    </p>
                 </div>
+            </div>
+            <div className="header-right">
+                <Dropdown />
             </div>
         </header>
     );
